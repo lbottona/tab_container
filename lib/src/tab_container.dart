724731aa9d6f825ab11a723060c5497dcbbbee54
leonardo.bottona <leonardo.bottona@studenti.unitn.it>
2024-02-08 10:34:16 +0100

    Ok is not right that way, revert

--- lib/src/tab_container.dart
+++ lib/src/tab_container.dart
@@ -399,21 +399,17 @@ class _TabContainerState extends AnimatedWidgetBaseState<TabContainer> {
       radius: widget.radius,
       child: Padding(
         padding: widget.childPadding,
-        child: ListView.builder(
-            scrollDirection: Axis.horizontal,
-            itemBuilder: (context, index) {
-              return AnimatedSwitcher(
-                duration: widget.childDuration!,
-                switchInCurve: widget.childCurve!,
-                transitionBuilder: widget.transitionBuilder ??
-                    AnimatedSwitcher.defaultTransitionBuilder,
-                child: IndexedStack(
-                  key: ValueKey<int>(_currentIndex),
-                  index: _currentIndex,
-                  children: widget.children,
-                ),
-              );
-            }),
+        child: AnimatedSwitcher(
+          duration: widget.childDuration!,
+          switchInCurve: widget.childCurve!,
+          transitionBuilder: widget.transitionBuilder ??
+              AnimatedSwitcher.defaultTransitionBuilder,
+          child: IndexedStack(
+            key: ValueKey<int>(_currentIndex),
+            index: _currentIndex,
+            children: widget.children,
+          ),
+        ),
       ),
       tabs: _getTabs(),
       tabExtent: _tabExtent,
